b5724d436ddb90f48448a16c52a5b116c9dd8cb0
Jason8792 <71681190+Jason8792@users.noreply.github.com>
2020-12-24 19:40:24 +0700

    Pom Bensin

---
+++ Pombensin/utility/PDO_util.php
@@ -0,0 +1,19 @@
+<?php
+
+    class PDO_util{
+        public static function createConnection(){
+            $link = new PDO("mysql:host=localhost; dbname=pombensin","root","");
+            $link->setAttribute(PDO::ATTR_AUTOCOMMIT,false);
+            $link->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            return $link;
+        }
+
+        public static function closeConnection(PDO $link){
+            if($link !=null){
+                $link=null;
+            }
+        }
+
+
+    }
+?>
---
+++ Pombensin/view/login.php
@@ -0,0 +1,22 @@
+<form method="POST">
+    <h1>Please Sign In</h1>
+    <div class="row row-space">
+        <div class="col-2">
+            <div class="input-group">
+                <label class="label">Username</label>
+                <input class="input--style-4" type="text" id="txtusername" name="txtusername" autofocus placeholder="username" >
+            </div>
+        </div>
+    </div>
+    <div class="row row-space">
+        <div class="col-2">
+            <div class="input-group">
+                <label class="label">Password</label>
+                <input class="input--style-4" type="password" id="txtpassword" name="txtpassword" autofocus required placeholder="password" >
+            </div>
+        </div>
+    </div>
+    <div class="p-t-15">
+        <input class="btn btn--radius-2 btn--blue" type="submit" value="Sign in" name="signin" />
+    </div>
+</form>
